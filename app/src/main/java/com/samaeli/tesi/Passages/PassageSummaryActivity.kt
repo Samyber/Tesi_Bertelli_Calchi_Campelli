a9eafc46ca30d8380e31d6f0330d4d8997c2d104
Samuele Bertelli <S4663386@studenti.unige.it>
2021-08-02 12:55:13 +0200

    Iniziato ad implementare PassageSummaryActivity.kt

--- app/src/main/java/com/samaeli/tesi/Passages/PassageSummaryActivity.kt
+++ app/src/main/java/com/samaeli/tesi/Passages/PassageSummaryActivity.kt
@@ -1,12 +1,25 @@
 package com.samaeli.tesi.Passages
 
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
+import androidx.annotation.RequiresApi
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.samaeli.tesi.R
 import com.samaeli.tesi.databinding.ActivityPassageSummaryBinding
 import com.samaeli.tesi.models.Passage
+import com.samaeli.tesi.models.User
+import com.squareup.picasso.Picasso
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.Year
+import java.time.temporal.ChronoUnit
 
 class PassageSummaryActivity : AppCompatActivity() {
 
@@ -17,6 +30,10 @@ class PassageSummaryActivity : AppCompatActivity() {
     }
 
     private var passage : Passage? = null
+
+    private var day : String? = null
+    private var month : String? = null
+    private var year : String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +51,9 @@ class PassageSummaryActivity : AppCompatActivity() {
         Log.d(TAG,"departure city: ${passage!!.departureCity}")
         Log.d(TAG,"arrival city: ${passage!!.arrivalCity}")
 
+        completeUserFields()
+        completePassageFields()
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -42,4 +62,71 @@ class PassageSummaryActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
+
+
+    private fun completeUserFields(){
+        val ref = FirebaseDatabase.getInstance().getReference("users/${passage!!.uid}")
+        ref.addListenerForSingleValueEvent(object : ValueEventListener{
+            @RequiresApi(Build.VERSION_CODES.O)
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val user = snapshot.getValue(User::class.java)
+                binding.nameUserTextViewPassageSummary.text = user!!.name+" "+user!!.surname
+
+                getDate(user.birthdayDate)
+
+                val now : LocalDate = LocalDate.now()
+                val birthday = LocalDate.of(year!!.toInt() ,month!!.toInt(),day!!.toInt())
+                val age = ChronoUnit.YEARS.between(birthday,now)
+                Log.d(TAG,"The age is $age")
+                binding.ageUserTextViewPassageSummary.text = getString(R.string.age)+": "+age.toString()
+
+                if(user.gender.equals("male")){
+                    binding.genderTextViewPassageSummary.text = "M"
+                    binding.genderTextViewPassageSummary.setTextColor(Color.BLUE)
+                }else{
+                    binding.genderTextViewPassageSummary.text = "F"
+                    binding.genderTextViewPassageSummary.setTextColor(Color.rgb(255,179,222))
+                }
+
+                if(!user.profileImageUrl.isNullOrEmpty()) {
+                    val url = user.profileImageUrl
+                    val targetImageView = binding.photoCircleImageViewPassageSummary
+                    Picasso.get().load(url).into(targetImageView)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+
+        })
+    }
+
+    private fun completePassageFields(){
+        var minuteString = passage!!.minute.toString()
+        if(minuteString.length == 1){
+            minuteString = "0"+minuteString
+        }
+        binding.hourTextViewPassageSummary.text = getString(R.string.hour)+": "+passage!!.hour+":"+minuteString
+        binding.numberPersonTextViewPassageSummary.text = getString(R.string.number_person)+": "+passage!!.numberPerson
+
+        binding.departureAddressTextViewPassageSummary.text = getString(R.string.address)+": "+passage!!.departureAddress
+        binding.departureCityTextViewPassageSummary.text = getString(R.string.city)+": "+passage!!.departureCity
+
+        binding.arrivalAddressTextViewPassageSummary.text = getString(R.string.address)+": "+passage!!.arrivalAddress
+        binding.arrivalCityTextViewPassageSummary.text = getString(R.string.city)+": "+passage!!.arrivalCity
+    }
+
+    private fun getDate(timestamp:Long){
+        val formatter_day = SimpleDateFormat("dd")
+        val formatter_month = SimpleDateFormat("MM")
+        val formatter_year = SimpleDateFormat("yyyy")
+
+        day = formatter_day.format(timestamp)
+        month = formatter_month.format(timestamp)
+        year = formatter_year.format(timestamp)
+
+        //Log.d(TAG,formatter_day.toString()+" "+formatter_month.toString()+" "+formatter_year.toString())
+        //return formatter.format(timestamp)
+    }
+
 }
